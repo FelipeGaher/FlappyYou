6032c037133398e573ca5ab07fdb66b74d77c18d
NiemandZero <felipegajardo88@gmail.com>
2017-01-23 00:20:35 -0300

    menu in progress

--- menu.js
+++ menu.js
@@ -4,12 +4,40 @@ function Menu() {
     this.actiated = false;
 
     this.show = function () {
-        //tint(255, 250)
-       
-       // fill(250);
-       // rect(10, 10, this.x, this.y);
+
+        var menuColor = color('rgba(180, 50, 50, 0.8)')
+        var botons = color('blue')
+        var letters = color('rgba(180, 50, 50, 1)')
+
+        noStroke();
+        fill(menuColor);
+        rect(10, 10, this.x, this.y);
         this.actiated = true;
-      //  tint(255, 255)
+
+        stroke(255);
+        fill(botons);
+        rect(100, 100, 200, 40);
+        noStroke();
+        fill(letters);
+        textSize(25);
+        text('START', 200, 130);
+        stroke(255);
+        fill(botons);
+        rect(100, 300, 200, 40);
+        noStroke();
+
+        fill(letters);
+        textSize(25);
+        text('CREDITS', 200, 330);
+        stroke(255);
+        fill(botons);
+        rect(100, 500, 200, 40);
+        noStroke();
+
+        fill(letters);
+        textSize(25);
+        text('EXIT', 200, 530);
+
     }
 
 
@@ -18,4 +46,3 @@ function Menu() {
 
     }
 }
- 
